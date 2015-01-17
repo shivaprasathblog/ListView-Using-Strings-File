e4b661d626d2aa3ab1259acf95448743b9b01ed1
Shivaprasath <shivaprasathsnippet@users.noreply.github.com>
2015-01-17 15:47:44 +0530

    Update MainActivity.java

--- src/com/example/listviewusingstringsfile/MainActivity.java
+++ src/com/example/listviewusingstringsfile/MainActivity.java
@@ -12,7 +12,9 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
+	//String of data is declared in values folder in strings file
 	String [] cars;
+	//Defining Listview
 	ListView lv;
 	
 
@@ -21,36 +23,35 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		//Referring the ListView
 		lv=(ListView)findViewById(R.id.listView1);
-				
+		//Binding the value from strings file 		
 		cars=getResources().getStringArray(R.array.cars);
-		
-		
+		//Adapter to used to adapt the value from datasource(here data source is from array), so we are using ArrayAdapter the array is of type string 	
 		ArrayAdapter<String>adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,cars);
+		//Setting the adapter to Listview
 		lv.setAdapter(adapter);
-		
+		//Creating listener for ListView , we are going to Click a item from ListView so we are using itemclick listener
 		lv.setOnItemClickListener(new OnItemClickListener()
 		{
-
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) 
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) 
+				
 			{
 				// TODO Auto-generated method stub
-				
+				//Getting the value of item clicked
 				String itemselected=arg0.getItemAtPosition(arg2).toString();
-				
+				//Showing the clicked item from in Toast
 				Toast.makeText(MainActivity.this,"Your favourite is "+itemselected,Toast.LENGTH_LONG).show();
 				
 			}
 		});
-		
-		
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
